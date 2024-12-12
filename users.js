5b2ca63df0fccabff9099dafaf4a39055ba0e6ad
Devendra Singh Solanki <93856267+WebDeveloperDev@users.noreply.github.com>
2024-12-12 16:23:36 +0530

    Create users.js

---
+++ users.js
@@ -0,0 +1,19 @@
+var mongoose=require('mongoose')
+// const url = "mongodb+srv://talk2devendrasolanki:JfVPHJkw75kvbY7u@cluster0.33jhnfk.mongodb.net/?retryWrites=true&w=majority";
+const url = "mongodb+srv://talk2devendrasolanki:JfVPHJkw75kvbY7u@cluster0.33jhnfk.mongodb.net/looter?retryWrites=true&w=majority";
+
+mongoose.connect(url)
+
+// mongoose.connect('mongodb://localhost/my_db')
+var accountSchema=mongoose.Schema({
+    name: String,
+    username: String,
+    password: String
+
+})
+var Account=mongoose.model("account",accountSchema)
+
+module.exports={
+    accountSchema:accountSchema,
+    account:Account
+}
